a0d00d1c005893a8735a78fec0c9269f0e678635
Jimmy D <jd9192016@gmail.com>
2019-03-08 23:21:50 -0500

    Ready for 1st peer review

--- js/app.js
+++ js/app.js
@@ -17,3 +17,19 @@ $(function(){
   $('.circlechart').circlechart();
 
 });
+
+
+var moreBtn = $("#readMore").click(function(){
+    $("#readMoreDots").hide();
+    $("#more").show();
+    if(moreBtn.text()==="More"){
+    moreBtn.text("Less");
+    }else{
+        moreBtn.text("More");
+        $("#more").hide();
+        $("#readMoreDots").show();
+    }
+});
+
+    
+    
